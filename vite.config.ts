e853faa3d7086f521edbf1fa4995cc107c75a937
clebertmiguel <euclebermiguel@gmail.com>
2026-09-08 17:23:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
+  vite: {
+    plugins: [
+      nitro({
+        preset: "node-server",
+      }),
+    ],
+  },
+
   tanstackStart: {
-       prerender: {
+    server: {
+      entry: "server",
+    },
+
+    prerender: {
       enabled: true,
       autoSubfolderIndex: true,
       autoStaticPathsDiscovery: true,
